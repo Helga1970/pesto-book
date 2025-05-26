a20d2bbc2b137ef159a682085b6e908dba461968
Helga1970 <olga.kostrikina@gmail.com>
2025-05-26 21:27:59 +0200

    Update check-referer-v2.js

--- netlify/edge-functions/check-referer-v2.js
+++ netlify/edge-functions/check-referer-v2.js
@@ -1,3 +1,7 @@
+[[edge_functions]]
+path = "/*"
+function = "check-referer-v2"
+
 export default async (request, context) => {
   const referer = request.headers.get('referer');
   const requestUrl = request.url;
@@ -11,9 +15,22 @@ export default async (request, context) => {
     'http://pro-culinaria.ru',
     'https://www.pro-culinaria.ru',
     'http://www.pro-culinaria.ru',
-    'pro-culinaria.ru',
-    'www.pro-culinaria.ru',
+    // 'pro-culinaria.ru', // ЭТО ЛИШНЕЕ, Т.К. refererUrl.origin всегда будет содержать протокол
+    // 'www.pro-culinaria.ru', // ЭТО ЛИШНЕЕ
+
+    // Добавляем домен самого Netlify сайта, чтобы он мог загружать свои ресурсы
+    'https://pesto-book.netlify.app',
+    'http://pesto-book.netlify.app', // В случае, если кто-то зайдёт по http
   ];
+
+  // Также можно добавить более гибкую проверку для своего же домена,
+  // если запросы идут напрямую к ресурсам без реферера
+  const requestOrigin = new URL(requestUrl).origin;
+  if (allowedReferers.includes(requestOrigin)) {
+      console.log('Request origin is allowed, skipping referer check.');
+      return context.next();
+  }
+
 
   if (referer) {
     try {
@@ -34,6 +51,10 @@ export default async (request, context) => {
       console.error("Invalid referer URL or parsing error:", referer, e);
     }
   } else {
+    // Если реферер отсутствует, нужно решить, что делать.
+    // Если это прямой заход на ресурс, то он будет заблокирован текущей логикой.
+    // Если это первый запрос к странице (и реферер отсутствует),
+    // то его нужно пропустить, что уже учтено выше `requestOrigin` проверкой.
     console.log('No referer header found.');
   }
 
